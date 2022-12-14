76468cf31aca5209d982b54ffafeac0d591c29fe
Revenko Vladislav <revad8@gmail.com>
2022-12-14 23:47:01 +0300

    добавил задания

--- HW/Program.cs
+++ HW/Program.cs
@@ -3,13 +3,42 @@
 
 namespace HW
 {
+    enum Semaphore
+    {
+        Red = 100, Yellow = 200, Green = 300
+
+    }
+
     internal class Program
     {
+        //Задание 3.4.3
 
 
         static void Main(string[] args)
         {
 
+            //Задание 3.6.8
+            double result = 10 % 3;
+            Console.WriteLine(result);
+
+            //Задание 3.7.4
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            
+            Console.Write("Enter your age: ");
+            byte age = checked((byte)int.Parse(Console.ReadLine()));
+            
+            Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+
+            Console.Write("What is your favorite day of week?: ");
+
+            DayOfWeek d = DayOfWeek(1);
+            Console.WriteLine(d);
+            Console.ReadKey();
+
+
+
+            // Задание с анкетой
             Console.Write("Введите ваше имя: ");
             string name = Console.ReadLine();
             
